e6bfe8d346df5cce23dc894c9ff0544a7cee3b5e
Andy Castle <masterchief_and@live.com>
2025-02-13 16:09:46 -0500

    Add project files.

---
+++ DataBaseToExcelAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using DataBaseToExcelAPI.Models;
+using System.Collections.Generic;
+
+namespace DataBaseToExcelAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ DataBaseToExcelAPI/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace DataBaseToExcelAPI.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Marks { get; set; }
+    }
+}
